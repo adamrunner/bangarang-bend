1b8ad24664a50e39a9d46602e493f7e728c36181
Adam Runner <adamrunner@gmail.com>
2016-01-07 21:20:58 -0800

    added rbenv and ruby-version

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,14 @@ lock '3.1.0'
 set :application, 'bangarang-bend'
 set :repo_url, 'git@github.com:adamrunner/bangarang-bend.git'
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
