5ee1b92b864313202754f447333452764d074596
Adam Runner <adamrunner@gmail.com>
2016-03-02 16:19:55 -0800

    updated deploy, bad configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,6 @@ set :rbenv_roles, :all # default value
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{public}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs, %w{public/images/uploads}
 # Default value for default_env is {}
