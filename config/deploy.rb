51e3b7c6866e2be06b5efde1cbd18a9890fd79da
Adam Runner <adamrunner@gmail.com>
2016-03-02 16:07:58 -0800

    updated deploy to symlink public folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,7 @@ set :rbenv_roles, :all # default value
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
+set :linked_dirs, %w{public}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
