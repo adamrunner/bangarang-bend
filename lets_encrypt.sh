a2a017c9ed0546b0b42a8c0179f99f6a1673f0fc
Adam Runner <adamrunner@gmail.com>
2016-06-30 20:03:10 -0700

    lets encrypt script

---
+++ lets_encrypt.sh
@@ -0,0 +1,2 @@
+letsencrypt-auto renew --webroot -w /home/deploy/bangarang-bend/current/public/ -d www.bangarangbend.com -d bangarangbend.com -w /home/deploy/bangarang-bend-staging/current/public/ -d staging.bangarangbend.com
+letsencrypt-auto renew --webroot -w /home/deploy/bangarang-bend/current/public/
