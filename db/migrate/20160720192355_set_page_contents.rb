3aa2a8811914f05ea8d6a0e8a0b05969449ea73a
Bryan Runner <bryanrunner@gmail.com>
2016-08-01 13:17:19 -0700

    hopefully fix migration issue

--- db/migrate/20160720192355_set_page_contents.rb
+++ db/migrate/20160720192355_set_page_contents.rb
@@ -1,20 +1,23 @@
 class SetPageContents < ActiveRecord::Migration
+  Page.reset_column_information
+  
   home = Page.find_by(name: 'home')
+  services = Page.find_by(name: 'services')
+  catering_menus = Page.find_by(name: 'catering_menus')
+  events = Page.find_by(name: 'events')
+  philosophy = Page.find_by(name: 'philosophy')
+  food_truck = Page.find_by(name: 'food_truck')
+
   home.content << 'featured_items' << 'biography_items' << 'landings'
 
-  services = Page.find_by(name: 'services')
   services.content << 'service_items'
 
-  catering_menus = Page.find_by(name: 'catering_menus')
   catering_menus.content << 'catering_menus'
 
-  events = Page.find_by(name: 'events')
   events.content << 'event_items' << 'event_produce_items'
 
-  philosophy = Page.find_by(name: 'philosophy')
   philosophy.content << 'philosophy_items'
 
-  food_truck = Page.find_by(name: 'food_truck')
   food_truck.content << 'food_truck_image_rows' << 'food_truck_addresses'
 
   pages = [home, services, catering_menus, events, philosophy, food_truck]
